d9bd838e3429af81bf1a3476551674468fa0d52f
AlRemes Github <alpo.remes@gmail.com>
2022-02-02 16:17:33 +0200

    Test

---
+++ Alposbookstore/src/test/java/com/example/Alposbookstore/AlposbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Alposbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlposbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
